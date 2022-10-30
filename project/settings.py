2ee300f74843c756d8dedfc4d0bc0f32312b4d91
yone-m <masarres109foot@gmail.com>
2022-10-30 18:19:06 +0900

    contactの追加

--- project/settings.py
+++ project/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'myprofile.apps.MyprofileConfig',
     'diaries.apps.DiariesConfig',
     'reviews.apps.ReviewsConfig',
+    'contact.apps.ContactConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
